34a88a0bfbe9854ba230a0be5194b21505b2bc5a
syiffanoriza <syiffa.noriza@gmail.com>
2024-03-26 21:15:43 +0700

    Setting Up Admin Routes

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,20 @@ Route::get('/navbar-component', function () {
     return view('components.navbar');
 });
 
+// ADMIN ROUTES - - - -
+
 Route::get('/admin', function () {
     return view('admin.login');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard.beranda');
+});
+
+Route::get('admin/dashboard/produk', function() {
+    return view('admin.dashboard.produk.landing');
+});
+
+Route::get('admin/dashboard/produk/test', function() {
+    return view('admin.dashboard.produk.produk');
 });
