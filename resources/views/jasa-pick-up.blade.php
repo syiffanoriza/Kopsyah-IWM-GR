9062c9e934f99f263f18990d0980c9c016d1b99b
kiazunnurain <zaskiazunsch@gmail.com>
2024-05-11 14:02:33 +0700

    Sektor Jasa Pages

--- resources/views/jasa-pick-up.blade.php
+++ resources/views/jasa-pick-up.blade.php
@@ -110,7 +110,8 @@
                     <img src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
                         class="object-cover h-full w-full rounded-lg border border-gray-900" alt="">
                 </div>
-                <a href="http://wa.me/6282311996767" target="_blank"
+                <!-- Modal toggle -->
+                <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" type="button"
                     class="flex text-white bg-gradient-to-r from-[#7AD800] to-[#00A500] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-primary-300 shadow shadow-primary-900/30 font-medium rounded-lg text-md px-32 lg:px-14 py-2.5 text-center lg:mt-16 mt-5 me-2 mb-2 tracking-wide"><svg
                         xmlns="http://www.w3.org/2000/svg" class="w-5 mr-2 text-white" fill="currentColor"
                         viewBox="0 0 16 16">
@@ -118,7 +119,106 @@
                             d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
                     </svg></span>
                     Pesan Sekarang
-                </a>
+                </button>
+
+                <!-- Main modal -->
+                <div id="crud-modal" tabindex="-1" aria-hidden="true"
+                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative p-4 w-full max-w-md max-h-full">
+                        <!-- Modal content -->
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <!-- Modal header -->
+                            <div class="flex items-center p-4 md:p-5 border-b border-primary-500 rounded-t">
+                                <h3 class="text-2xl font-semibold text-gray-700">
+                                    Pesan Jasa <span class="text-primary-900">Pick Up</span>
+                                </h3>
+                                <button type="button"
+                                    class="text-gray-400 bg-transparent hover:bg-primary-200 hover:text-primary-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                                    data-modal-toggle="crud-modal">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <form>
+                                <div
+                                    class="h-80 overflow-y-auto border rounded-lg m-5 md:p-5 border-primary-500 p-4 grid gap-4 mb-4 grid-cols-2">
+                                    <div class="col-span-2">
+                                        <label for="name"
+                                            class="block mb-2 text-sm font-medium text-primary-900">Nama</label>
+                                        <input type="text" name="name" id="name"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-900 focus:border-primary-900 block w-full p-2.5"
+                                            placeholder="Tulis nama lengkap anda..." required="">
+                                    </div>
+                                    <div class="col-span-2">
+                                        <label for="number"
+                                            class="block mb-2 text-sm font-medium text-primary-900">Nomor
+                                            Telepon</label>
+                                        <input type="number" name="number" id="number"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-900 focus:border-primary-900 block w-full p-2.5 "
+                                            placeholder="Nomor telepon yang dapat dihubungi" required="">
+                                    </div>
+                                    <div class="col-span-2">
+                                        <label for="keperluan"
+                                            class="block mb-2 text-sm font-medium text-primary-900">Keperluan
+                                            Peminjaman</label>
+                                        <input type="text" name="number" id="number"
+                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-900 focus:border-primary-900 block w-full p-2.5 "
+                                            placeholder="e.g., AC Kotor, perlu dibersihkan" required="">
+                                    </div>
+                                    <div class="col-span-2">
+                                        <label for="date"
+                                            class="block mb-2 text-sm font-medium text-primary-900">Durasi
+                                        </label>
+                                        <div date-rangepicker class="flex items-center">
+                                            <div class="relative">
+                                                <input name="start" type="date"
+                                                    class="bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                                                    placeholder="Select date start">
+                                            </div>
+                                            <span class="mx-2 text-gray-500">to</span>
+                                            <div class="relative">
+                                                <input name="end" type="date"
+                                                    class="bg-gray-50 border border-gray-300 text-gray-500 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
+                                                    placeholder="Select date end">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-span-2">
+                                        <label for="address"
+                                            class="block mb-2 text-sm font-medium text-primary-900">Alamat Pengambilan
+                                        </label>
+                                        <textarea id="description" rows="4"
+                                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-900 focus:border-primary-900 "
+                                            placeholder="Tulis alamat anda..."></textarea>
+                                    </div>
+                                    <div class="col-span-2">
+                                        <form class="max-w-lg mx-auto">
+                                            <label class="block mb-2 text-sm font-medium text-primary-900"
+                                                for="user_avatar">Upload Foto KTP</label>
+                                            <input
+                                                class="block w-full text-sm text-gray-500 border border-gray-300 accent-primary-900 rounded-lg cursor-pointer bg-gray-50focus:outline-none "
+                                                aria-describedby="user_avatar_help" id="user_avatar" type="file">
+                                        </form>
+                                    </div>
+                                </div>
+                                <button type="submit"
+                                    class="justify-center text-white bg-gradient-to-r from-[#7AD800] to-[#00A500] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-primary-300 shadow shadow-primary-900/30 font-medium rounded-lg inline-flex items-center bg-primary-900 hover:bg-primary-800 text-sm px-5 py-2.5 mx-5 mb-5 mt-2 text-center">
+                                    <svg class="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                                    </svg>
+                                    Pesan Sekarang
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
