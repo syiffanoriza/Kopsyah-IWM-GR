8d00e1770d9952b0885937c59523c588ebfb04a6
syiffanoriza <syiffa.noriza@gmail.com>
2024-06-02 14:00:08 +0700

    Route Reformat

--- routes/web.php
+++ routes/web.php
@@ -28,115 +28,38 @@ Route::middleware('user.data')->group(function () {
         // Layanan Kami
 
         // Sektor Jasa
-        Route::view('/sektor-jasa', '');
+        Route::view('/sektor-jasa', 'sektor-jasa/sektor-jasa');
+        Route::view('/jasa-laundry', 'sektor-jasa/jasa-laundry');
+        Route::view('/jasa-service-ac', 'sektor-jasa/jasa-service-ac');
+        Route::view('/jasa-pick-up', 'sektor-jasa/jasa-pick-up');
+
+        // Sektor Simpanan
+        Route::view('/sektor-simpanan', 'sektor-simpanan/sektor-simpanan');
+        Route::view('/simpanan-berjangka', 'sektor-simpanan/simpanan-berjangka');
+        Route::view('/simpanan-qurban', 'sektor-simpanan/simpanan-qurban');
+        Route::view('/simpanan-umroh', 'sektor-simpanan/simpanan-umroh');
+
+        // Sektor Pembiayaan Pages
+        Route::view('/sektor-pembiayaan', 'sektor-pembiayaan/sektor-pembiayaan');
+        Route::view('/pembiayaan-konsumtif', 'sektor-pembiayaan/pembiayaan-konsumtif');
+        Route::view('/pembiayaan-modal-umkm', 'sektor-pembiayaan/pembiayaan-modal-umkm');
+
+        // Proyek Tahunan Pages
+        Route::view('/proyek-tahunan', 'proyek-tahunan/proyek-tahunan');
+        Route::view('/penjualan-ramadhan-fair', 'proyek-tahunan/penjualan-ramadhan-fair');
+        Route::view('/hewan-qurban', 'proyek-tahunan/hewan-qurban');
+
+        // Perdagangan Pages
+        Route::view('/belanja', 'sektor-perdagangan/belanja');
+        Route::view('/katalog-produk', 'sektor-perdagangan/katalog-produk');
+        Route::view('/cart-belanja', 'sektor-percart-belanja');
+        Route::view('/checkout-belanja', 'sektor-perdagangan/checkout-belanja');
+
+        // Daftar Pages
+        Route::view('/daftar', 'daftar-kopsyah/daftar');
+        Route::view('/form-daftar', 'daftar-kopsyah/form-daftar');
     });
 });
-
-// Layanan Kami Pages 
-
-//Sektor Jasa Pages
-
-Route::get('/sektor-jasa', function () {
-    return view('/sektor-jasa/sektor-jasa');
-});
-
-Route::get('/jasa-laundry', function () {
-    return view('/sektor-jasa/jasa-laundry');
-});
-
-Route::get('/jasa-service-ac', function () {
-    return view('/sektor-jasa/jasa-service-ac');
-});
-
-Route::get('/jasa-pick-up', function () {
-    return view('/sektor-jasa/jasa-pick-up');
-});
-
-//Sektor Simpanan Pages
-
-Route::get('/sektor-simpanan', function () {
-    return view('/sektor-simpanan/sektor-simpanan');
-});
-
-Route::get('/simpanan-berjangka', function () {
-    return view('/sektor-simpanan/simpanan-berjangka');
-});
-
-Route::get('/simpanan-qurban', function () {
-    return view('/sektor-simpanan/simpanan-qurban');
-});
-
-Route::get('/simpanan-umroh', function () {
-    return view('/sektor-simpanan/simpanan-umroh');
-});
-
-// Sektor Pembiayaan Pages
-
-Route::get('/sektor-pembiayaan', function () {
-    return view('/sektor-pembiayaan/sektor-pembiayaan');
-});
-
-Route::get('/pembiayaan-konsumtif', function () {
-    return view('/sektor-pembiayaan/pembiayaan-konsumtif');
-});
-
-Route::get('/pembiayaan-modal-umkm', function () {
-    return view('/sektor-pembiayaan/pembiayaan-modal-umkm');
-});
-
-// Proyek Tahunan Pages
-
-Route::get('/proyek-tahunan', function () {
-    return view('/proyek-tahunan/proyek-tahunan');
-});
-
-Route::get('/penjualan-ramadhan-fair', function () {
-    return view('/proyek-tahunan/penjualan-ramadhan-fair');
-});
-
-Route::get('/hewan-qurban', function () {
-    return view('/proyek-tahunan/hewan-qurban');
-});
-
-// Perdagangan Pages
-
-Route::get('/belanja', function () {
-    return view('sektor-perdagangan/belanja');
-});
-
-Route::get('/katalog-produk', function () {
-    return view('sektor-perdagangan/katalog-produk');
-});
-
-Route::get('/cart-belanja', function () {
-    return view('sektor-perdagangan/cart-belanja');
-});
-
-Route::get('/checkout-belanja', function () {
-    return view('sektor-perdagangan/checkout-belanja');
-});
-
-// Daftar Pages
-
-Route::get('/daftar', function () {
-    return view('/daftar-kopsyah/daftar');
-});
-
-Route::get('/form-daftar', function () {
-    return view('/daftar-kopsyah/form-daftar');
-});
-
-// Components
-
-Route::get('/navbar-component', function () {
-    return view('components.navbar');
-});
-
-Route::middleware('auth')->group(function () {
-    // Input routes for user dashboard...
-});
-
-// User Routes
 
 Route::middleware(['auth', 'role.user'])->group(function () {
     Route::controller(UserController::class)->group(function () {
