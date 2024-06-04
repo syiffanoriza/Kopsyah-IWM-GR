5611505fa4c3f2d4c3eb93f8c091650e6aa13c39
syiffanoriza <syiffa.noriza@gmail.com>
2024-06-04 13:47:30 +0700

    Setting up admin dashboard sidebar

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,20 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index() {
-        return view('admin.dashboard.beranda');
+        return view('admin.dashboard');
     }
-
-    public function login() {
-        return view('admin.login');
-    }
-
-    public function produk() {
-        return view('admin.dashboard.produk.landing');
-    }
-
-    public function sembako() {
-        return view('admin.dashboard.produk.produk');
-    }
-
-
 }
